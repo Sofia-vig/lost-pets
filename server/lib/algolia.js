4e10fdd88d0ab2519522d7a7cfa0867404d65024
sofia <sofiavign@gmail.com>
2021-10-30 15:48:04 +0200

    probando??

--- server/lib/algolia.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.indexPets = exports.indexUsers = void 0;
-var algoliasearch_1 = require("algoliasearch");
-var client = (0, algoliasearch_1["default"])("0UE5CAIWZS", "0e457674481462a143776a96da183cd5");
-var indexUsers = client.initIndex("users");
-exports.indexUsers = indexUsers;
-var indexPets = client.initIndex("pets");
-exports.indexPets = indexPets;
